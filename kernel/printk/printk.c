f324510e5c224707a0c95aca7169e10d150da7d6
n010529 <hongwei.di@archermind.com>
2018-12-10 19:44:21 +0800

    Bug Id:1249220:     [AMT][power][P][WW]40%以下开启寿命倍增模式充电至80%后重开机后电量掉至70%(3rd) PRODUCT_NAME:ZC553KL-o Module:Android O System ROOT_CAUSE:Android O啓動時間長，導致開機概率性ftm分區掛載不上，之前保存的數據無法正常讀取 Solution:close log output Test: as title
    Change-Id: I13e79d04f8eeabd1be964a464f4b8e5de0efa3fd
    Signed-off-by: n010529 <hongwei.di@archermind.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -60,10 +60,10 @@ extern void printascii(char *);
 #endif
 
 int console_printk[4] = {
-	CONSOLE_LOGLEVEL_DEFAULT,	/* console_loglevel */
-	MESSAGE_LOGLEVEL_DEFAULT,	/* default_message_loglevel */
-	CONSOLE_LOGLEVEL_MIN,		/* minimum_console_loglevel */
-	CONSOLE_LOGLEVEL_DEFAULT,	/* default_console_loglevel */
+	0,//CONSOLE_LOGLEVEL_DEFAULT,	/* console_loglevel */
+	0,//MESSAGE_LOGLEVEL_DEFAULT,	/* default_message_loglevel */
+	0,//CONSOLE_LOGLEVEL_MIN,		/* minimum_console_loglevel */
+	0,//CONSOLE_LOGLEVEL_DEFAULT,	/* default_console_loglevel */
 };
 
 /* Deferred messaged from sched code are marked by this special level */
